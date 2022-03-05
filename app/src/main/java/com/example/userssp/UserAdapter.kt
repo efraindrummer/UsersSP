c12048a661fb97ed909b9e422a769d0bbb2bf3bc
Efrain May <efrain_may_75@hotmail.com>
2022-03-05 14:28:19 -0600

    view alternative

--- app/src/main/java/com/example/userssp/UserAdapter.kt
+++ app/src/main/java/com/example/userssp/UserAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.userssp.databinding.ItemUserAltBinding
 import com.example.userssp.databinding.ItemUserBinding
 
 class UserAdapter(private val users: List<User>, private val listener: OnClickListener) : RecyclerView.Adapter<UserAdapter.ViewHolder>(){
@@ -16,7 +17,7 @@ class UserAdapter(private val users: List<User>, private val listener: OnClickLi
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         context = parent.context
 
-        val view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.item_user_alt, parent, false)
 
         return ViewHolder(view)
     }
@@ -40,7 +41,7 @@ class UserAdapter(private val users: List<User>, private val listener: OnClickLi
     override fun getItemCount(): Int  = users.size
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-         val binding = ItemUserBinding.bind(view)
+         val binding = ItemUserAltBinding.bind(view)
 
         fun setListener(user: User, position: Int){
             binding.root.setOnClickListener {
